671b3cb186fb85a8a49aeaf59bec06e001a26b91
svpcadmin <PhilisPaxil@gmail.com>
2020-10-03 21:51:51 +0900

    add: astParser.ts

--- src/modules/ast/astParser/astParser.ts
+++ src/modules/ast/astParser/astParser.ts
@@ -1,3 +1,5 @@
-export const astParser = () => {
-    
-}
+import * as tsc from "typescript";
+
+export const astParser = (source: string) => {
+  return tsc.createSourceFile("", source, tsc.ScriptTarget.ES5, true);
+};
