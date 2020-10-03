d4c4c6433c760e31acd1e9506874227ed38f04e6
svpcadmin <PhilisPaxil@gmail.com>
2020-10-03 21:52:11 +0900

    add: main.ts to check how to work in astParser

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
-console.log(process.argv);
+import { join } from "path";
+import { astParser } from "./modules/ast/astParser/astParser";
+import { fileReader } from "./modules/file/fileReader/fileReader";
+
+import * as ts from "typescript"
+
+const entryPointSource = fileReader(join(__dirname, "../examples/SimpleButton/App.tsx"))
+
+console.log(astParser(entryPointSource))
