83303fe09cfd80dc8a6a31e0cd72b00cf40355b6
svpcadmin <PhilisPaxil@gmail.com>
2020-10-07 22:23:29 +0900

    fix: astParser to get CompilerOption

--- src/modules/ast/astParser/astParser.ts
+++ src/modules/ast/astParser/astParser.ts
@@ -2,9 +2,11 @@ import * as tsc from "typescript";
 
 export const astParser = (entry: string, config: tsc.CompilerOptions) => {
   const program = tsc.createProgram([entry], config);
-  const source = program.getSourceFile("");
+  const sources = program.getSourceFiles();
+  const source = sources.find(sourceEl => sourceEl.fileName.substr(-5) !== ".d.ts");
   if(!source) {
     throw new Error("astParser: source is undefined")
   }
+
   return source
 };
