4ccf5ff608a2455d38b903033fdc294a884a41e3
svpcadmin <PhilisPaxil@gmail.com>
2020-10-07 22:24:21 +0900

    add: TODO comment

--- src/modules/ast/astParser/astParser.ts
+++ src/modules/ast/astParser/astParser.ts
@@ -3,6 +3,7 @@ import * as tsc from "typescript";
 export const astParser = (entry: string, config: tsc.CompilerOptions) => {
   const program = tsc.createProgram([entry], config);
   const sources = program.getSourceFiles();
+  // TODO is this right way to find SourceFile object of entry file .ts ?
   const source = sources.find(sourceEl => sourceEl.fileName.substr(-5) !== ".d.ts");
   if(!source) {
     throw new Error("astParser: source is undefined")
