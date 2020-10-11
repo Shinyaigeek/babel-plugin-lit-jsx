5f8c376366ea97e2f51920b89f81754cf59cd36d
shinyaigeek <PhilisPaxil@gmail.com>
2020-10-11 23:31:53 +0900

    fix: isEventListener

--- packages/jsx2templateLiteral/src/modules/ast/convertTsx2TemplateLiteral/isEventListener/isEventListener.ts
+++ packages/jsx2templateLiteral/src/modules/ast/convertTsx2TemplateLiteral/isEventListener/isEventListener.ts
@@ -3,9 +3,17 @@ import { JSXAttribute, JSXElement } from "@babel/types";
 import { isUserDefinedComponent } from "../../../util/isUserDefinedComponent/isUserDefinedComponent";
 
 export const isEventListener = (attr: NodePath<JSXAttribute>) => {
-  if (attr.parentPath.node.type !== "JSXElement") {
+  if (
+    attr.parentPath.node.type !== "JSXElement" &&
+    attr.parentPath.node.type !== "JSXOpeningElement" &&
+    typeof attr.node.name.name !== "string"
+  ) {
     // TODO Why node.type infered as Node ?
     throw new Error("invalid node type");
   }
-  return isUserDefinedComponent(attr.parentPath as NodePath<JSXElement>);
+  return (
+    !isUserDefinedComponent(attr.parentPath as NodePath<JSXElement>) &&
+    (attr.node.name.name as string)[0] === "o" &&
+    (attr.node.name.name as string)[1] === "n"
+  );
 };
