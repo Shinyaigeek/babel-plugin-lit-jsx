8e96d883ad9f1aaac2e09e70672b962696688a0e
shinyaigeek <PhilisPaxil@gmail.com>
2020-10-11 22:50:39 +0900

    add: isEventListener

--- packages/jsx2templateLiteral/src/modules/ast/convertTsx2TemplateLiteral/isEventListener/isEventListener.ts
+++ packages/jsx2templateLiteral/src/modules/ast/convertTsx2TemplateLiteral/isEventListener/isEventListener.ts
@@ -1,3 +1,11 @@
-export const isEventListener = (listener: string) => {
-  return listener.length > 2 && listener[0] === "o" && listener[1] === "n";
+import { NodePath } from "@babel/traverse";
+import { JSXAttribute, JSXElement } from "@babel/types";
+import { isUserDefinedComponent } from "../../../util/isUserDefinedComponent/isUserDefinedComponent";
+
+export const isEventListener = (attr: NodePath<JSXAttribute>) => {
+  if (attr.parentPath.node.type !== "JSXElement") {
+    // TODO Why node.type infered as Node ?
+    throw new Error("invalid node type");
+  }
+  return isUserDefinedComponent(attr.parentPath as NodePath<JSXElement>);
 };
