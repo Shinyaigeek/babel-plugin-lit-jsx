15b129df5bbd17aa9f7bc21b478fbcc13a66c6e1
shinyaigeek <PhilisPaxil@gmail.com>
2020-10-12 23:39:09 +0900

    add: compileEachFile

---
+++ packages/jsx2templateLiteral/src/modules/ast/compileEachFile/compileEachFile.ts
@@ -0,0 +1,11 @@
+import { astParser } from "../astParser/astParser";
+import { codegen } from "../codegen/codegen";
+import { convertTsx2TemplateLiteral } from "../convertTsx2TemplateLiteral/convertTsx2TemplateLiteral";
+
+export const compileEachFile = (target: string) => {
+  const ast = astParser(target);
+
+  const result = convertTsx2TemplateLiteral(ast);
+
+  console.log(codegen(ast));
+};
