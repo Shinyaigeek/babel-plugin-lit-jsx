7430ab2601e6d3e5b804d8fcae7770235b36f917
shinyaigeek <PhilisPaxil@gmail.com>
2020-10-16 18:21:09 +0900

    fix: typeof accessRootProgramRecursively

--- packages/jsx2templateLiteral/src/modules/ast/accessRootProgramRecursively/accessRootProgramRecursively.ts
+++ packages/jsx2templateLiteral/src/modules/ast/accessRootProgramRecursively/accessRootProgramRecursively.ts
@@ -1,13 +1,13 @@
 import { NodePath } from "@babel/traverse";
-import { isProgram } from "@babel/types";
+import { isProgram, Program } from "@babel/types";
 
 export const accessRootProgramRecursively = (
   entry: NodePath<any>
-): NodePath<ParentNode> => {
+): NodePath<Program> => {
   const parentNode = entry.parentPath;
   if (isProgram(parentNode.node)) {
     // TODO type guardでparentNode.nodeがParentNodeだからparentNodeはNodePath<ParentNode>って推論できない
-    return (parentNode as any) as NodePath<ParentNode>;
+    return parentNode as NodePath<Program>;
   }
 
   return accessRootProgramRecursively(parentNode);
