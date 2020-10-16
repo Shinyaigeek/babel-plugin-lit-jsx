270cac7246c6b1e54a198d96c2ce39ed3c205225
shinyaigeek <PhilisPaxil@gmail.com>
2020-10-16 18:30:36 +0900

    add: insert html import logic into converting JSX into templateLiteral

--- packages/jsx2templateLiteral/src/modules/ast/convertTsx2TemplateLiteral/convertReturnedJSXElementToString/convertReturnedJSXElementToString.ts
+++ packages/jsx2templateLiteral/src/modules/ast/convertTsx2TemplateLiteral/convertReturnedJSXElementToString/convertReturnedJSXElementToString.ts
@@ -8,7 +8,9 @@ import {
   templateElement,
   templateLiteral,
 } from "@babel/types";
+import { accessRootProgramRecursively } from "../../accessRootProgramRecursively/accessRootProgramRecursively";
 import { ConvertJSXElementToTemplateLiteral } from "../convertJsxElementToTemplateLiteral/convertJsxElementToTemplateLiteral";
+import { insertHTMLImport } from "../insertHtmlImport/insertHTMLImport";
 import { tagHtmlPrefix } from "../tagHtmlPrefix/tagHtmlPrefix";
 
 //TODO later
@@ -21,6 +23,8 @@ export const convertReturnedJSXElementToString = (
       nodePath.node.argument
     );
     Convert.traverse();
+    const rootNode = accessRootProgramRecursively(nodePath);
+    insertHTMLImport(rootNode);
     nodePath.replaceWith(returnStatement(tagHtmlPrefix(Convert.render())));
   } else {
     console.warn("pure functional component should return jsx element");
