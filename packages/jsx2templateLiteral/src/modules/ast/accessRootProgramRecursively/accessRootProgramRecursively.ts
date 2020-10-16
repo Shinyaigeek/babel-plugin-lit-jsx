f6dca82e6a041a8884f1675fee020475c236ca6e
shinyaigeek <PhilisPaxil@gmail.com>
2020-10-16 17:07:42 +0900

    add: accessRootProgramRecursively

---
+++ packages/jsx2templateLiteral/src/modules/ast/accessRootProgramRecursively/accessRootProgramRecursively.ts
@@ -0,0 +1,14 @@
+import { NodePath } from "@babel/traverse";
+import { isProgram } from "@babel/types";
+
+export const accessRootProgramRecursively = (
+  entry: NodePath
+): NodePath<ParentNode> => {
+  const parentNode = entry.parentPath;
+  if (isProgram(parentNode.node)) {
+    // TODO type guardでparentNode.nodeがParentNodeだからparentNodeはNodePath<ParentNode>って推論できない
+    return (parentNode as any) as NodePath<ParentNode>;
+  }
+
+  return accessRootProgramRecursively(parentNode);
+};
