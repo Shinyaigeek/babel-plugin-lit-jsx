92118c2ece433835d782bf69101f3fd58bc66324
shinyaigeek <PhilisPaxil@gmail.com>
2020-10-18 02:56:25 +0900

    add: test case for convertJsxElementToTemplateLiteral

---
+++ packages/jsx2templateLiteral/src/modules/ast/convertTsx2TemplateLiteral/convertJsxElementToTemplateLiteral/__tests__/SimpleJsx.tsx
@@ -0,0 +1,1 @@
+export const SimpleJsx = () => <div>asdf</div>;
--- packages/jsx2templateLiteral/src/modules/ast/convertTsx2TemplateLiteral/convertJsxElementToTemplateLiteral/convertJsxElementToTemplateLiteral.test.ts
+++ packages/jsx2templateLiteral/src/modules/ast/convertTsx2TemplateLiteral/convertJsxElementToTemplateLiteral/convertJsxElementToTemplateLiteral.test.ts
@@ -1,8 +1,44 @@
+import {
+  arrowFunctionExpression,
+  file,
+  identifier,
+  isJSXElement,
+  program,
+  variableDeclaration,
+  variableDeclarator,
+} from "@babel/types";
+import { join } from "path";
+import { codegen } from "../../codegen/codegen";
+import { extractJsxElementFromSouceFile } from "../../extractJsxElementFromSouceFile/extractJsxElementFromSourceFile";
 import { ConvertJSXElementToTemplateLiteral } from "./convertJsxElementToTemplateLiteral";
 
 describe("convertJsxElementToTemplateLiteral", () => {
   test("simple jsx", () => {
+    const jsx = extractJsxElementFromSouceFile(
+      join(__dirname, "./__tests__/SimpleJsx.tsx")
+    )[0];
 
-    // const converter = new ConvertJSXElementToTemplateLiteral(jsx);
+    if (!isJSXElement(jsx)) {
+      throw new Error("simple jsx is not jsx element");
+    }
+
+    const converter = new ConvertJSXElementToTemplateLiteral(jsx!);
+
+    converter.traverse();
+
+    const raw = codegen(
+      file(
+        program([
+          variableDeclaration("const", [
+            variableDeclarator(
+              identifier("SimpleJsx"),
+              arrowFunctionExpression([], converter.render())
+            ),
+          ]),
+        ])
+      )
+    );
+
+    expect(raw).toBe("const SimpleJsx = () => `<div>asdf</div>`;");
   });
 });
