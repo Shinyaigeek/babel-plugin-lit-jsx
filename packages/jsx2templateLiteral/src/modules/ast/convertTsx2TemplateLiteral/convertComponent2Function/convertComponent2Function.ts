cb95fa568d8a33f6c975c584d8be79ef38ec7585
shinyaigeek <PhilisPaxil@gmail.com>
2020-10-20 22:14:17 +0900

    feature(jsx2templateLiteral): add support for jsx spread attribute props

--- packages/jsx2templateLiteral/src/modules/ast/convertTsx2TemplateLiteral/convertComponent2Function/convertComponent2Function.ts
+++ packages/jsx2templateLiteral/src/modules/ast/convertTsx2TemplateLiteral/convertComponent2Function/convertComponent2Function.ts
@@ -1,15 +1,20 @@
 import { NodePath } from "@babel/traverse";
 import {
   callExpression,
+  Identifier,
   identifier,
   isJSXAttribute,
   isJSXIdentifier,
+  isJSXSpreadAttribute,
   jsxElement,
   JSXElement,
   jsxExpressionContainer,
   JSXIdentifier,
+  memberExpression,
   objectExpression,
+  ObjectProperty,
   objectProperty,
+  spreadElement,
   stringLiteral,
 } from "@babel/types";
 import { getTagNameFromElement } from "../../../util/getTagNameFromElement/getTagNameFromElement";
@@ -19,24 +24,37 @@ import { resolveAttrValue } from "../resolveAttrValue/resolveAttrValue";
 export const convertComponent2Function = (nodePath: NodePath<JSXElement>) => {
   if (isUserDefinedComponent(nodePath.node)) {
     const tagName = getTagNameFromElement(nodePath.node);
+    const spreadAttributes = nodePath.node.openingElement.attributes.find(
+      (attr) => isJSXSpreadAttribute(attr)
+    );
     nodePath.replaceWith(
       callExpression(identifier(tagName), [
-        objectExpression(
-          nodePath.node.openingElement.attributes.map((attr) => {
-            //TODO
-            if (isJSXAttribute(attr)) {
-              if (isJSXIdentifier(attr.name)) {
-                return objectProperty(
-                  identifier(attr.name.name),
-                  resolveAttrValue(attr.value)
-                );
-              } else {
-                throw new Error("jsx named space is not supported");
-              }
-            } else {
-              throw new Error("spread attribute is not supported");
-            }
-          })
+        callExpression(
+          memberExpression(identifier("Object"), identifier("assign")),
+          [
+            objectExpression(
+              nodePath.node.openingElement.attributes
+                .map((attr) => {
+                  //TODO
+                  if (isJSXAttribute(attr)) {
+                    if (isJSXIdentifier(attr.name)) {
+                      return objectProperty(
+                        identifier(attr.name.name),
+                        resolveAttrValue(attr.value)
+                      );
+                    } else {
+                      throw new Error("jsx named space is not supported");
+                    }
+                  }
+                })
+                .filter(
+                  (attr): attr is ObjectProperty => typeof attr !== "undefined"
+                )
+            ),
+            isJSXSpreadAttribute(spreadAttributes)
+              ? identifier((spreadAttributes.argument as Identifier).name)
+              : objectExpression([]),
+          ]
         ),
       ])
     );
