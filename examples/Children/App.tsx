8b09d3cef26611536e3ef3b3c0db248c8fbd3e12
shinyaigeek <PhilisPaxil@gmail.com>
2020-10-20 21:59:54 +0900

    fix(examples): example behavior

--- examples/Children/App.tsx
+++ examples/Children/App.tsx
@@ -2,10 +2,15 @@ import { Footer } from "./Footer";
 import { Header } from "./Header";
 import { Layout } from "./Layout";
 
+const hoge = {
+  bar: "bar",
+  fuga: "fuga",
+};
+
 export const App = () => {
   return (
     <div>
-      <Layout Header={<Header />} Footer={<Footer />} />
+      <Layout Header={<Header />} Footer={<Footer />} {...hoge} />
     </div>
   );
 };
--- examples/Children/Layout.tsx
+++ examples/Children/Layout.tsx
@@ -3,11 +3,13 @@ import { ReactElement } from "react";
 interface Props {
   Header: ReactElement;
   Footer: ReactElement;
+  bar: string;
+  fuga: string;
 }
 
 export const Layout = (props: Props) => {
   return (
-    <div>
+    <div className={props.bar} id={props.fuga}>
       {props.Header}
       {props.Footer}
     </div>
